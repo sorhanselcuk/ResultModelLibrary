bc75ea7d6c2c632792a313a8b3fdd285610110c1
Şuayip Orhan Selçuk <s.orhanselcuk@gmail.com>
2025-01-16 16:50:04 +0300

    DefaultDataResult modeli eklenmiştir.

---
+++ Concrete/DefaultDataResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResultModelLibrary.Concrete
+{
+    public class DefaultDataResult<T> : DataResult<T>
+    {
+        public DefaultDataResult(string message): base(Activator.CreateInstance<T>(), false, message)
+        {
+
+        }
+    }
+}
